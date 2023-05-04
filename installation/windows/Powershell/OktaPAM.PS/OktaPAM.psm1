58ecb7dccaaec63364b38471d311bf20ae847414
Shad Lutz <shad.lutz@okta.com>
2023-05-04 08:43:06 -0600

    Adding powershell server-tools install module

---
+++ installation/windows/Powershell/OktaPAM.PS/OktaPAM.psm1
@@ -0,0 +1,136 @@
+<# 
+ .Synopsis
+  Installs the OktaPAM Server Toools
+
+ .Description
+  Installs the OktaPAM Server Tools to a specific version, and if installing for the first time
+  will set the configuration file with various values.
+
+ .Parameter ToolsVersion
+  Optional, version of the OktaPAM Server Tools to Install. If not supplied, the latest available
+  version is installed.
+
+ .Parameter EnrollmentToken
+  Optionally sets the OktaPAM Enrollment Token 
+
+ .Example
+   # Installs the OktaPAM Server Agent
+   Install-OktaPamServerTools -ToolsVersion 1.22.1
+#>
+
+function Get-URL-With-Authenticode(){
+    param(
+        [Parameter(Mandatory=$true)][string]$url,
+        [Parameter(Mandatory=$true)][string]$output
+    )
+    process{
+        $baseDir = Split-Path -Parent -Path $output
+
+        if (!(Test-Path "$output")) {
+            echo "Downloading $url to $output"
+            New-Item -force -path $baseDir -type directory
+            Invoke-WebRequest -UserAgent "OktaPAM/PS1-0.1.1" -UseBasicParsing -TimeoutSec 30 -Uri $url -OutFile $output
+        } else {
+            echo "Existing $output found"
+        }
+
+        $sig = Get-AuthenticodeSignature $output
+
+        if ($sig.Status -ne "Valid") {
+            echo "error: signature for $($output) is invalid: $($sig.Status) from $($sig.SignerCertificate.ToString())"
+            throw "error: signature for $($output) is invalid: $($sig.Status) from $($sig.SignerCertificate.ToString())"
+        }
+
+        echo "$($output) is signed by OktaPAM"
+    }
+}
+
+function Stop-OktaPAMService(){
+    $installed = [bool](Get-Service -ErrorAction SilentlyContinue | Where-Object Name -eq "OktaPAM-server-tools")
+    if ($installed -eq $true) {
+        echo "Stoping Service OktaPAM-server-tools"
+        Stop-Service -Name "OktaPAM-server-tools"
+        return $true
+    }
+    return $false
+}
+
+function Start-OktaPAMService(){
+    $installed = [bool](Get-Service -ErrorAction SilentlyContinue | Where-Object Name -eq "OktaPAM-server-tools")
+    if ($installed -eq $true) {
+        echo "Starting Service OktaPAM-server-tools"
+        Start-Service -Name "OktaPAM-server-tools"
+        return $true
+    }
+    return $false
+}
+
+
+
+function Install-OktaPamServerTools(){
+    param(
+        [Parameter(Mandatory=$false)][string]$EnrollmentToken,
+        [Parameter(Mandatory=$false)][string]$ToolsVersion
+    )
+    process{
+        $ErrorActionPreference = "Stop";
+        # Check that the function is being run as administator.
+        if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            Write-Error "This command must be run as an administrator."
+            return
+        }
+        $installBaseUrl = "https://dist.OktaPAM.com/repos/windows/stable/amd64/server-tools"
+        $jsonUrl = "$($installBaseUrl)/dull.json"
+        $jsonData = Invoke-RestMethod -Uri $jsonUrl
+        $latestRelease = $jsonData.releases[0]
+        $latestVersion = $latestRelease.version
+        $latestInstallerLink = ($latestRelease.links | where { $_.rel -eq "installer" }).href
+        
+        if ($PSBoundParameters.ContainsKey("ToolsVersion")) {
+            $installerURL = "$($installBaseUrl)/v$($ToolsVersion)/OktaPAM-Server-Tools-$($ToolsVersion).msi"
+        } else {
+            $installerURL = "$($installBaseUrl)/$($latestInstallerLink)"
+        }
+
+        # Select Local System User, where the OktaPAM Server Agent Runs
+        $systemprofile = (Get-CIMInstance win32_userprofile  | where-object sid -eq "S-1-5-18" | select -ExpandProperty localpath)
+        $stateDir = Join-Path $systemprofile -ChildPath 'AppData' | Join-Path -ChildPath "Local" | Join-Path -ChildPath "OktaPAM"
+    
+        if ($PSBoundParameters.ContainsKey("EnrollmentToken")) {
+            $tokenPath = Join-Path $stateDir -ChildPath "enrollment.token"
+            New-Item -ItemType directory -Path $stateDir -force
+            $EnrollmentToken | Out-File $tokenPath -Encoding "ASCII" -Force
+        }
+
+        $msiPath = [System.IO.Path]::ChangeExtension([System.IO.Path]::GetTempFileName(), '.msi')
+        $msiLog = [System.IO.Path]::ChangeExtension($msiPath, '.log')
+
+        Get-URL-With-Authenticode -url $installerURL -output $msiPath
+
+        $stopped = Stop-OktaPAMService
+
+        trap {
+            if ($stopped -eq $true) {
+                Start-OktaPAMService
+            }
+            break
+        }
+
+        echo "Starting msiexec on $($msiPath)"
+        echo "MSI Log path: $($msiLog)"
+
+        $status = Start-Process -FilePath msiexec -ArgumentList /i,$msiPath,/qn,/L*V!,$msiLog  -Wait -PassThru
+
+        if ($status.ExitCode -ne 0) {
+	        Start-OktaPAMService
+            throw "msiexec failed with exit code: $($status.ExitCode) Log: $($msiLog)"
+        }
+
+        echo "Removing $($msiPath)"
+        Remove-Item -Force $msiPath
+
+        Start-OktaPAMService
+    }
+}
+
+Export-ModuleMember -function Install-OktaPamServerTools
---
+++ installation/windows/Powershell/README.md
@@ -0,0 +1,57 @@
+# OktaPAM-PowerShell: Tools for installing OktaPAM (ASA) ServerTools with PowerShell
+
+**_These modules are not supported by Okta, and no warranty is expressed or implied.  Please review and understand all modules before using.  Use at your own risk._**
+
+## Available Functions
+
+### `Install-OktaPamServerTools`
+
+This function will download and install the OktaPAM Server Tools.  It can optionally set the enrollment token.  If the OktaPAM Server Tools are already installed, this function will upgrade them to the latest or specified version.
+
+## Prerequisites
+
+These modules are tested under PowerShell version 7
+
+## Installation
+
+1. Copy the entire module folder, OktaPAM.PS, into 'C:\Program Files (x86)\WindowsPowerShell\Modules'
+
+```Powershell
+mkdir 'C:\Program Files (x86)\WindowsPowerShell\Modules\OktaPAM.PS'
+cd 'C:\Program Files (x86)\WindowsPowerShell\Modules\OktaPAM.PS'
+curl -O https://github.com/Okta-PAM-Resource-Kit/scripts/tree/main/installation/windows/powershell/OktaPAM.PS/*
+```
+
+2. Verify the module is available
+
+```Powershell
+Get-Module -ListAvailable
+```
+
+3. Once verified, import the module
+
+```Powershell
+Import-Module -Name OktaPAM.PS -Force
+```
+
+## Usage
+
+**_Functions from this module must be run as administrator._**
+
+``` Powershell
+Install-OktaPamServerTools [-ToolsVersion <version>] [-EnrollmentToken <enrollment_token>]
+#    -ToolsVersion        Installs the specified version of the software (mininum 1.66.4)
+#                         Version must be in the form of n.nn.n
+#                         If ommitted, the lastest available version will be used
+#    -EnrollmentToken     Creates an enrollment token file using the provided token value
+#                         For upgrades, there is no need to provide an enrollment token.
+```
+
+## What's New
+
+### 0.1.1
+
+* Updated to use new repository structure as of 2023Q1.
+* Removed support for non-stable release trains
+* Removed support for custom instances
+
