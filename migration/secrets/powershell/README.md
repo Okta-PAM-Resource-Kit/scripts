cb42d3e30bd2cadb3f546cd41e979777662a3e28
Shad Lutz <shad.lutz@okta.com>
2024-04-18 08:51:53 -0600

    Adding secrets migration example

---
+++ migration/secrets/powershell/README.md
@@ -0,0 +1,18 @@
+Powershell-Script to migrate creds to Okta Privileged Access Vault
+This is a basic script that demonstrates adding credentials to the OPA Vault using the sft command line tool. 
+Preferred way is to use an API endpoint to read credentials from the source and adding it to OPA vault.
+NOTE: The scripts provided here are as is and not officially supported by Okta. 
+
+
+Usage
+This project provides an example PowerShell script to the following actions:
+- Read credentials from a csv.
+- call sft secrets command to add credentials to OPA vault.
+
+
+Prerequisites
+The base prerequisite for these script is Microsoft Powershell
+
+Built With
+Microsoft Powershell
+
---
+++ migration/secrets/powershell/opa_cred_migration.ps1
@@ -0,0 +1,28 @@
+# This file contains functions with sample code. To use one, call it.
+# Read secrets from CSV and create in Okta Vault
+
+
+<# Call API endpoint from source vault to extract creds real-time... (preferred)
+OR
+CSV
+Sample CSV file. Make sure you include the header line as the first record.
+resourceGroup,project,path,key,value,name,description
+Jo_DevelopmentResources,Secrets,OktaApiKeys,cyberArk1,testIng001,cyberArkSafe1,secrets from Safe1
+#>
+function CreateSecrets() {
+
+    $secrets = Import-Csv Secrets.csv
+    foreach ($secret in $secrets) {
+		
+         $secretProfile = @{resourceGroup = $secret.resourceGroup; project = $secret.project; path = $secret.path; key = $secret.key; value = $secret.value; name = $secret.name; description = $secret.description}
+        #$groupIds = $user.groupIds -split ";"
+		Write-Output $SecretProfile
+		sft secrets create --resource-group $secretProfile.resourceGroup --project $secretProfile.project --path $secretProfile.path --key $secretProfile.key --value $secretProfile.value --name $secretProfile.name  --description $secretProfile.description
+
+    }
+	
+	Write-Output "Completed Secrets Mangement at $(Get-Date)"
+
+}
+
+CreateSecrets
